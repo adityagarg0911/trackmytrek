5ca6dc9f0a1b0ddfe0c4bb5d103b2d2eab24e020
adityagarg0911 <adityagarg0911@gmail.com>
2024-01-21 23:10:09 +0530

    added Client and Server side Error Handling and Data Validation

---
+++ schemas.js
@@ -0,0 +1,12 @@
+// IT CONTAINS SCHEMA FOR VALIDATION OF DATA FOR POST AND PUT REQUEST WITH THE HELP OF JOI
+const Joi = require('joi');
+
+module.exports.campgroundSchema = Joi.object({
+    campground: Joi.object({
+        title: Joi.string().required(),
+        price: Joi.number().required().min(0),
+        image: Joi.string().required(),
+        location: Joi.string().required(),
+        description: Joi.string().required()
+    }).required()
+});
